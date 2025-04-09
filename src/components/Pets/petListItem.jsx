c62e4bb44841928a45365d1dfc2c972e8a3aeb27
meanmachine90 <75108532+meanmachine90@users.noreply.github.com>
2025-04-08 18:41:56 -0600

    aj update

--- src/components/Pets/petListItem.jsx
+++ src/components/Pets/petListItem.jsx
@@ -1,21 +1,14 @@
-export const PetListItem = ({ name, cod, img, type, width, height, breads,onShowDetail }) => {
+export const PetListItem = ({ name, cod, img, type, width, height, breads, onShowDetail }) => {
     return (
-        <div  onClick={onShowDetail}>
+        <div onClick={onShowDetail}>
             <img src={img} alt={name} width={width} height={height} />
             <div>
-<<<<<<< Updated upstream
-                <h3>{name} ({type})</h3>
-                <p>ID: {cod}</p>
-                <button onClick={()=>{onShowDetail(cod)}}>¡Adopta!</button>
-=======
-
                 <div>
-                <strong>{name} </strong>
-                <p><strong>({type} )</strong></p>
+                    <strong>{name} </strong>
+                    <p><strong>({type} )</strong></p>
                     <p>ID: {cod}</p>
-                    <button onClick={()=>{onShowDetail(cod)}}  >¡Adopta!</button>
+                    <button onClick={() => { onShowDetail(cod) }}  >¡Adopta!</button>
                 </div>
->>>>>>> Stashed changes
             </div>
         </div>
     );
